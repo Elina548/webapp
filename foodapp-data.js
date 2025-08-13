59fd90cd5143f7b66217c68dffa151818d20ba1e
Elina548 <elinaangel155@gmail.com>
2025-08-13 20:17:42 +0530

    Add files via upload

---
+++ foodapp-data.js
@@ -0,0 +1,18 @@
+const restaurants = [ 
+{ 
+id: 1, 
+name: "A2B", 
+menu: [ 
+{ id: 1, name: "Mini Meals", price: 120, image: "photos/meals.avif" }, 
+{ id: 2, name: "Masal Dosa", price: 92, image: "photos/dosa.jpeg" } 
+] 
+}, 
+{  
+id: 2, 
+name: "McD", 
+menu: [ 
+{ id: 3, name: "Veggie Burger", price: 199, image: "photos/burger.jpeg" }, 
+{ id: 4, name: "Chicken Burger", price: 249, image: "photos/shutterstock_574607542.jpg" }
+]
+}
+];
